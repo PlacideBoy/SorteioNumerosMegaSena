30efb31d8302dc791ceae8890ec2f4f8fada79a5
JorgeLucas <83237763+PlacideBoy@users.noreply.github.com>
2023-07-20 16:30:57 -0300

    Initial commit

---
+++ SorteioNumerosMegaSena/Principal.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SorteioNumerosMegaSena
+{
+    partial class FrmMegaSena
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(331, 184);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(357, 232);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(25, 13);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "......";
+            // 
+            // FrmMegaSena
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnGerar);
+            this.Name = "FrmMegaSena";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mega Sena";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ SorteioNumerosMegaSena/Principal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SorteioNumerosMegaSena
+{
+    public partial class FrmMegaSena : Form
+    {
+        public FrmMegaSena()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            Sorteio();
+        }
+
+        private void Sorteio()
+        {
+            Random sorteio = new Random();
+            int sorteado;
+            int[] verificar = new int[61];
+
+            string resultado = "";
+            for (int i = 0; i < 6;  i++)
+            {
+                inicio: 
+                sorteado = sorteio.Next(1, 61);
+
+                for (int x = 0; x < 6; x++)
+                {
+                    if (verificar[x] == sorteado)
+                    {
+                        goto inicio;
+                    }
+                }
+
+                resultado = resultado + "  " + sorteado;
+                lblResultado.Text = resultado;
+                verificar[i] = sorteado;
+            }
+
+        }
+
+    }
+}
